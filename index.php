547df2d2572ae9bc21334170463d8dcd79744093
bsc-inf-04-19 <bsc-inf-04-19@unima.ac.mw>
2023-02-01 11:31:13 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Student Management System</title>
+        <link rel="stylesheet" type="text/css" href="Style.css">
+        
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav>
+        <label class="logo">INFORMARTICS</label>
+
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="" class="btn btn-success">Login</a></li>
+
+</nav>
+
+<div class="section1">
+
+<label class="img_text">We Teach Students With Care</label>
+    <img class= "main_img" src="studentmanagemt.jpg">
+</div>
+
+<div class="container">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <img class ="image" src="school.jpg">
+</div>
+
+<div class="col-md-8">
+<h1>Welcome to Informatics</h1>
+
+<p class="paragraph"> Unima has been commited to global learning long before it became n indispensble
+    feature of contemporary. Established in 1964, we proudly stand as the 1st Nyasaland 
+    medium school in Southern Africa to adopt both person edexcel and international curriculum
+    (in O and A levels), drawing together students in a vibrant, academically,challenging and
+    encouraging and encouraging environment where manifold viewpoints are prized and
+    celebrated. 
+    Unima has been commited to global learning long before it became n indispensble
+    feature of contemporary. Established in 1964, we proudly stand as the 1st Nyasaland 
+    medium school in Southern Africa to adopt both person edexcel and international curriculum
+    (in O and A levels), drawing together students in a vibrant, academically,challenging and
+    encouraging and encouraging environment where manifold viewpoints are prized and
+    celebrated. 
+    </p>
+</div>
+
+</div>
+</div>
+<center>
+    <h1>Our Teacher</h1>
+</center>
+<div class="container">
+<div class="row">
+    <div class="col-md-4">
+        <img class="school" src="school1.jpg">
+        <p>
+            in a vibrant, academically challenging, and 
+            encouraging environment where manifold viewpoints
+            are prized and celebrated.
+        </p>
+</div>
+<div class="col-md-4">
+<img class="school" src="school2.jpg">
+<p>
+            in a vibrant, academically challenging, and 
+            encouraging environment where manifold viewpoints
+            are prized and celebrated.
+        </p>
+</div>
+<div class="col-md-4">
+<img class="school" src="school3.jpg">
+<p>
+            in a vibrant, academically challenging, and 
+            encouraging environment where manifold viewpoints
+            are prized and celebrated.
+        </p>
+</div>
+</div>
+</div>
+
+
+
+<center>
+    <h1>Our Courses</h1>
+</center>
+<div class="container">
+<div class="row">
+    <div class="col-md-4">
+        <img class="school" src="c1.jpg">
+        <h3>Web Design</h3></div>
+
+<div class="col-md-4">
+<img class="school" src="c2.jpg">
+<h3>Graphics designing</h3>
+</div>
+
+<div class="col-md-4">
+<img class="school" src="c3.jpg">
+<h3>Marketing</h3>
+</div>
+</div>
+</div>
+
+<center>
+    <h1 class="adm">Admission Form</h1>
+</center>
+<div align="center" class="admission_form">
+    <form>
+        <div class="adm_int">
+            <label class="label_text">Name</label>
+            <input class="input_deg" type="text" name="">
+         </div>
+
+         <div>
+            <label class="label_text">Email</label>
+            <input class="input_deg" type="text" name="">
+         </div>
+
+         <div>
+            <label class="label_text">Phone </label>
+            <input class="input_deg" type="text" name="">
+         </div>
+
+         <div>
+            <label>Message</label>
+            <textarea class="input_txt"></textarea>
+         </div>
+
+         <div>
+            <input class ="btn btn-primary" id="submit" value="apply">
+         </div>
+</form>
+
+</div>
+
+<footer>
+    <h3 class="footer_text">All rights @copyright reserved by web tech knowledge</h3>
+</footer>
+
+</body>
+</html>
